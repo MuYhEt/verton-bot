3ec74c553a489fbecc50aa9b98ba038bff37a0c0
MuYhEt <pedrovzambi@outlook.com>
2017-10-17 21:32:33 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ bot.on("message", function(message) {
         var embed = new Discord.RichEmbed()
         .addField("ID", message.author.id)
         .addField("USERNAME", message.author.tag)
-        .addTitle(message.author.tag)
+        .setTitle(message.author.tag)
         //.setDescription(message.author.username + "'s profile")
         .setFooter("discord.gg/qrgAPGU")
         .setThumbnail(message.author.avatarURL)
