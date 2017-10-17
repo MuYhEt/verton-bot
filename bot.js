6edd8e86f330e06fcadb53bdfd095dccd7e92cc6
MuYhEt <pedrovzambi@outlook.com>
2017-10-17 21:13:50 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ bot.on("message", function(message) {
             var embed = new Discord.RichEmbed()
             .addField("About us", "Some text")
             .addField("Prefix", PREFIX, true)
-            .addField("Commands", "Ping, 8ball")
+            .addField("Commands", "Ping, 8ball",true)
             .setDescription("Developers Lab - Community")
            .setFooter("Our discord - discord.gg/qrgAPGU")
             .setThumbnail(message.author.avatarURL)
